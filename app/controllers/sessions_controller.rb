0e49fbef23d96cedbb731acb5975e2d634c6d5b2
aki <akihirohigasa@AKIHIROnoMacBook-Pro.local>
2019-11-25 17:36:15 +0900

    flashメッセージ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash[:danger] =  'Invalid email/password combination' # 本当は正しくない
+    end
+      render 'new'
   end
 
   def destroy
