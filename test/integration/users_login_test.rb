503507a58ca68bac91bab24efc32ecc3a24d220a
aki <akihirohigasa@AKIHIROnoMacBook-Pro.local>
2019-11-28 09:20:53 +0900

    10章終了

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,7 +7,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with invalid information" do
     get login_path
-    #assert_template 'sessions/new'
+    assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
     #assert_template 'sessions/new'
     assert_not flash.empty?
@@ -22,7 +22,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
-    #assert_template 'users/show'
+    assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
@@ -36,6 +36,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+
   end
 
   test "login with remembering" do
@@ -48,7 +49,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     log_in_as(@user, remember_me: '1')
     delete logout_path
     # クッキーを削除してログイン
-    #log_in_as(@user, remember_me: '0')
-    #assert_empty cookies['remember_token']
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
