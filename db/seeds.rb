d9db443057edadc943364523e4d251833d10d26e
aki <akihirohigasa@AKIHIROnoMacBook-Pro.local>
2019-12-02 19:30:46 +0900

    13.2.2 マイクロポストのサンプル

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,9 @@ User.create!(name:  "有村　架純",
                activated: true,
                activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
